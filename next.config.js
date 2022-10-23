24b7c75725d2b604563182f7315df16fa60070b0
Rino Reji Cheriyan <Rino.RejiCheriyan@gds.ey.com>
2022-10-23 20:16:42 +0530

    nextjs app init commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
