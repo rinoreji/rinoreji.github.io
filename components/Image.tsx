43f2102abc2e15355adeb63fa1f44c3011243f7e
Rino Reji Cheriyan <Rino.RejiCheriyan@gds.ey.com>
2022-10-24 22:00:06 +0530

    try4 build fix -  package and img tag update

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,6 +1,6 @@
 import NextImage, { ImageProps } from 'next/image'
 
-// eslint-disable-next-line @next/next/no-img-element
-const Image = ({ ...rest }: ImageProps) => <img {...rest} />
+// eslint-disable-next-line @next/next/no-img-element, @typescript-eslint/no-explicit-any, jsx-a11y/alt-text
+const Image = ({ ...rest }: any) => <img {...rest} />
 
 export default Image
