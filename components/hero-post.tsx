ea6fbf05ca37436a8753a8c9e586c5e642bb076f
Rino Reji Cheriyan <Rino.RejiCheriyan@gds.ey.com>
2022-10-24 12:49:18 +0530

    added styles, simplified layout

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -23,10 +23,10 @@ const HeroPost = ({
 }: Props) => {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
+      {/* <div className="mb-8 md:mb-16">
         <CoverImage title={title} src={coverImage} slug={slug} />
-      </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+      </div> */}
+      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-10 md:mb-14">
         <div>
           <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
             <Link as={`/posts/${slug}`} href="/posts/[slug]">
@@ -39,7 +39,7 @@ const HeroPost = ({
         </div>
         <div>
           <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          <Avatar name={author.name} picture={author.picture} />
+          {/* <Avatar name={author.name} picture={author.picture} /> */}
         </div>
       </div>
     </section>
