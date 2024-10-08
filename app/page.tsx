e7dd103b9f3ef99d17206c05d17faf390a2ea1cc
Rino Reji <mailrino@gmail.com>
2024-10-08 15:47:21 +0530

    calling an api

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  const getProducts = async ()=>{
+    const res = await fetch('https://dummyjson.com/products');
+    return res.json();
+  };
+
+  const data = await getProducts();
+  console.log('data len',data.products.length);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -12,16 +21,15 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <ul className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          {
+            data.products.map((item:any,index:any) => 
+              <li className="mb-2" key={index}>
+                {item.title}
+              </li>
+            )
+          }
+        </ul>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
